c78f88139031ec6b6cf585258058338797a4adf2
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-12-22 15:44:36 +0000

    fixing bug in models.py

--- home/models.py
+++ home/models.py
@@ -5,9 +5,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
-    name = models.Charfield(max_length=254)
-    friendly_name = models.Charfield(max_length=254, null=True, blank=True)
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -17,7 +17,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.Foreignkey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -25,3 +25,6 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
