3217ce5690fccda06ecb384ed260b611cb9a9136
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-01-03 12:56:31 +0000

    adding search queries to products with Q funtion

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -8,7 +10,17 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-
+    query = None
+  
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "you didn't enter any search criteria!")
+                return redirect(reverse('products/'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
     context = {
         'products': products,
     }
@@ -23,6 +35,7 @@ def product_detail(request, product_id):
 
     context = {
         'product': product,
+        'search_term': query
     }
 
     return render(request, 'products/product_detail.html', context)
