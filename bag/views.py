501f136ac1f73da5ae8e566da1fd5dc432d35ec5
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-01-17 15:14:15 +0000

    adding jquery core from cdn, encountered bug

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -35,3 +33,53 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ adjust the quantity of a specific product """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_id'][size] = quantity
+        else:
+            del bag[item_id]['items_by_id'][size]
+            if not bag[item_id]['items_by_id']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ remove the item from the shopping bag """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            if quantity > 0:
+                del bag[item_id]['items_by_id'][size]
+            if not bag[item_id]['items_by_id']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
