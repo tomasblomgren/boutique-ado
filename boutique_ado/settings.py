6506716e439989095eb2a90c8adef5455054bf60
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-01-25 12:00:24 +0000

    adding checkout label and input fields in checkout.html

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +58,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -72,6 +77,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags'
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
