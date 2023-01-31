df3351780372421109a5d5f64c6e57b5bca1553c
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-01-31 15:20:07 +0000

    added clean data forloop in webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,7 +20,25 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details  # updated
+        shipping_details = intent.shipping
+        grand_total = round(stripe_charge.amount / 100, 2)  # updated
+
+        # clean data in the shipping fields
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
