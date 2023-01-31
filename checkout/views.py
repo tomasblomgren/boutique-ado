7cd8d9766c6f9af9a7e27569c0f1c94ec12b999e
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-01-31 19:18:48 +0000

    finalizing the stripe setup with time delays to prevent event errors

--- checkout/views.py
+++ checkout/views.py
@@ -11,21 +11,20 @@ from bag.contexts import bag_contents
 import stripe
 import json
 
-
 @require_POST
 def cache_checkout_data(request):
     try:
-        pid = require.POST.get('client_secret').split('_secret')[0]
+        pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
-            'username': request.user
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'sorry your payment cant be processed at this time \
-            please try again later')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -49,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
