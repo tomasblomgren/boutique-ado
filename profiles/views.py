49f2b30aad7d56e181fe3799337b679e8bdc3c08
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-02-06 10:11:57 +0000

    attempting to fix bug in checkout/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
-from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
-
     template = 'profiles/profile.html'
     context = {
-        'form': form,
-        'orders': orders,
+        'profile': profile,
     }
 
     return render(request, template, context)
