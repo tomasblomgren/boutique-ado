70aa16d284ce011a95a024f32e9c74a731714ab4
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-02-10 13:05:08 +0000

    adding edit product.html and edit_product in views.py

--- products/views.py
+++ products/views.py
@@ -92,3 +92,17 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing { product.name }')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+    return render(request, template, context)
