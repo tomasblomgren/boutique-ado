71403ef7043bd173ad9807f257b51a461d566c79
tomasblomgren <Tomas.blomgren96@gmail.com>
2023-02-21 09:50:09 +0000

    added secret_key and debug settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '&933rh=gf1iez%_zo5zwpz6irgp5375njfatejse9hc$ny#0n7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tomasblomgren-boutique_ado.herokuapp.com', 'localhost']
 
